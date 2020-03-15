07df0582438a1682c3251387f7a5b7d61393d2a6
junichi-fujita <fujita0909j@gmail.com>
2020-03-15 23:39:56 +0900

    最寄り駅を空欄で登録した際にからのレコードが追加されないように変更

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -1,4 +1,5 @@
 class RealEstate < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
-  accepts_nested_attributes_for :nearest_stations
+  accepts_nested_attributes_for :nearest_stations,
+    reject_if: lambda { |attributes| attributes['route_name'].blank? }
 end
