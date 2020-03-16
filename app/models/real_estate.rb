a90bf6e135ae4515b479c40b9efdc6deefea3c83
junichi-fujita <fujita0909j@gmail.com>
2020-03-16 20:16:07 +0900

    real_esteteにバリデーションを追加

--- app/models/real_estate.rb
+++ app/models/real_estate.rb
@@ -2,4 +2,10 @@ class RealEstate < ApplicationRecord
   has_many :nearest_stations, dependent: :destroy
   accepts_nested_attributes_for :nearest_stations,
     reject_if: lambda { |attributes| attributes['route_name'].blank? }
+
+  validates :real_estate_name, presence: true
+  validates :rent, presence: true
+  validates :address, presence: true
+  validates :age, presence: true
+  validates :remarks, presence: true
 end
